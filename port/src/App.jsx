5f4754a53d3e5bcdbb3635a6757643e84baf4596
Tewolde Tesfaye <tolatesfaye30@gmail.com>
2026-04-11 14:14:43 +0300

    all addede

---
+++ port/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import About from './components/About/About'
+import Hero from './components/Hero/Hero'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+return (
+  <>
+  <Navbar/>
+  <Hero/>
+  <About/>
+  
+  </>
+)
+}
+
+export default App
